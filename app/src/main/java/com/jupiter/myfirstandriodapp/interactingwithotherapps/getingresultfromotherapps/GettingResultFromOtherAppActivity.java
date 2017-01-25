f281bf11ae1768929f13c991faeb0fbb95478600
Nigel Tang (haijutan) <haijutan@cisco.com>
2017-01-25 15:53:19 +0800

    permission setting

--- app/src/main/java/com/jupiter/myfirstandriodapp/interactingwithotherapps/getingresultfromotherapps/GettingResultFromOtherAppActivity.java
+++ app/src/main/java/com/jupiter/myfirstandriodapp/interactingwithotherapps/getingresultfromotherapps/GettingResultFromOtherAppActivity.java
@@ -1,9 +1,13 @@
 package com.jupiter.myfirstandriodapp.interactingwithotherapps.getingresultfromotherapps;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +19,7 @@ import org.w3c.dom.Text;
 
 public class GettingResultFromOtherAppActivity extends AppCompatActivity {
     private static final int PICK_CONTACT_REQUEST_CODE = 1;
+    private static final int PERMISSION_READ_CONTACTS_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +28,16 @@ public class GettingResultFromOtherAppActivity extends AppCompatActivity {
     }
 
     public void onGetOneContact(View view) {
-        Intent intent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
-        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
-        startActivityForResult(intent,PICK_CONTACT_REQUEST_CODE);
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        if(PackageManager.PERMISSION_GRANTED != permissionCheck) {
+            // Should we show an explanation?
+            if(!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, PERMISSION_READ_CONTACTS_REQUEST_CODE);
+            }
+            return;
+        }
+        getOneContact();
+
     }
 
     @Override
@@ -42,5 +54,20 @@ public class GettingResultFromOtherAppActivity extends AppCompatActivity {
                 textView.setText(number);
             }
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
+        if(PERMISSION_READ_CONTACTS_REQUEST_CODE == requestCode){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getOneContact();
+            }
+        }
+    }
+
+    private void getOneContact(){
+        Intent intent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(intent,PICK_CONTACT_REQUEST_CODE);
     }
 }
